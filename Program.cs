8beb24d06d4120628d783a677b4104e6a4a2de02
monista <ecrseer>
2021-04-14 00:29:12 -0300

    n

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Edge.SeleniumTools;
+
+namespace badotryEdge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+
+            // Launch Microsoft Edge (Chromium)
+            var options = new EdgeOptions();
+            options.UseChromium = true;
+            var drver = new EdgeDriver(options);
+            drver.Url="https://mbasic.facebook.com/";
+		    drver.Navigate();
+            Console.WriteLine("Start mobile mode and type enter");
+        }
+    }
+}
